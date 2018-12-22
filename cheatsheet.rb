867e83aae7885e9b7b7c8f622f7bd61cad0bec86
jakowicz <simon.jakowicz@gmail.com>
2018-12-22 01:30:39 +0000

    Update cheatsheet.rb

--- cheatsheet.rb
+++ cheatsheet.rb
@@ -1,9 +1,9 @@
 
 class Cheatsheet < Formula
-  desc ""
-  homepage ""
-  url "https://github.com/jakowicz/cheatsheet/archive/v1.0.1.tar.gz"
-  sha256 "0d7aeb34a847009903deac04aa9f0b1695709d4d5ad4960f54cb4fdf0e79ef1f"
+  desc "Cheatsheets galore"
+  homepage "https://github.com/jakowicz/cheatsheet"
+  url "https://github.com/jakowicz/cheatsheet/archive/v1.0.0.tar.gz"
+  sha256 "b3855ccb8c57828fe0383c7f1dc732b2a77b24a1286e9c8f525bd7b21ef6b1cc"
 
   def install
     bin.install "cheatsheet.sh"
