7bef9482b4abe928cbac7081b89df0cec872ef2d
jakowicz <simon.jakowicz@gmail.com>
2018-12-31 01:39:17 +0000

    Update recent.rb

--- recent.rb
+++ recent.rb
@@ -1,7 +1,7 @@
 class Cheatsheet < Formula
   desc Recent shell history "
   homepage "https://github.com/jakowicz/recent"
-  url "https://github.com/jakowicz/cheatsheet/archive/v1.0.0.tar.gz"
+  url "https://github.com/jakowicz/recent/archive/v1.0.0.tar.gz"
   sha256 "ad116ed83b3bb44a0302fc3e94da44bc4784c51b940646c4b40c97186651d419"
 
   def install
