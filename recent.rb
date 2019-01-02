79bc0fe15edae2c3f001d4ab7741bbd0752c6d84
jakowicz <simon.jakowicz@gmail.com>
2019-01-02 10:59:40 +0000

    Update recent.rb

--- recent.rb
+++ recent.rb
@@ -1,8 +1,8 @@
 class Recent < Formula
   desc "Recent shell history"
   homepage "https://github.com/jakowicz/recent"
-  url "https://github.com/jakowicz/recent/archive/v1.0.0.tar.gz"
-  sha256 "ad116ed83b3bb44a0302fc3e94da44bc4784c51b940646c4b40c97186651d419"
+  url "https://github.com/jakowicz/recent/archive/v1.0.1.tar.gz"
+  sha256 "86daeb97c57604b037bd4e3a656761892aa4216be994d2c74d73233390a2b077"
 
   def install
     bin.install "recent.py"
